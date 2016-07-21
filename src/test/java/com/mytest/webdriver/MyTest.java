18ac0e4276cd5c3946a98b1fb4b7992f9f3091bf
Ellina <frolova.ellina@gmail.com>
2016-07-21 09:30:43 +0300

    Update MyTest.java

--- src/test/java/com/mytest/webdriver/MyTest.java
+++ src/test/java/com/mytest/webdriver/MyTest.java
@@ -36,7 +36,7 @@ public class MyTest {
         driver.findElement(By.id("signin-password")).sendKeys("123456");
         driver.findElement(By.xpath("//*[@id=\"front-container\"]/div[2]/div[2]/form/table/tbody/tr/td[2]/button")).click();
         String text = driver.findElement(By.className("message-text")).getText();
-//        Assert.assertEquals(text,"Введённые адрес электронной почты и пароль не совпадают с сохранёнными в нашей базе данных. Проверьте правильность введённых данных и повторите попытку.");
+        Assert.assertEquals(text,"Введённые адрес электронной почты и пароль не совпадают с сохранёнными в нашей базе данных. Проверьте правильность введённых данных и повторите попытку.");
     }
 
     @AfterClass
