1762289ba6253e263d43051822f2a2739dc6a192
Ellina <dancalya@gmail.com>
2016-07-25 16:28:46 +0300

    created testng.xml added support of testng.xml added plugins in pom.xml

--- src/test/java/com/mytest/webdriver/MyTest.java
+++ src/test/java/com/mytest/webdriver/MyTest.java
@@ -1,50 +1,25 @@
 package com.mytest.webdriver;
 
-import java.io.File;
-import java.io.IOException;
 import com.maps.uiMap;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeDriverService;
+import org.testng.annotations.*;
 import org.testng.annotations.Test;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
-import org.testng.annotations.BeforeTest;
 import org.testng.Assert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
-
-
-import java.io.IOException;
 import java.util.concurrent.TimeUnit;
-//import org.openqa.selenium.chrome.ChromeDriver;
+
 /**
  * Created by USER on 20.07.2016.
  */
 
+
 public class MyTest {
-    private static ChromeDriverService service;
+
     private WebDriver driver;
 
- /*   @BeforeClass
-    public static void createAndStartService() throws IOException {
-        service = new ChromeDriverService.Builder()
-                .usingDriverExecutable(new File(System.getProperty("user.dir") + "\\seleniumdrivers\\chromedriver.exe"))
-                .usingAnyFreePort()
-                .build();
-        service.start();
-    }
-
-
-    @BeforeTest
-        public  void SetUP() {
-//          driver=new FirefoxDriver();
-//        System.setProperty("webdriver.chrome.driver",System.getProperty("user.dir")+"\\seleniumdrivers\\chromedriver.exe");
-//        driver=new ChromeDriver();
-
-            driver = new ChromeDriver(service);
-    }*/
 
     @BeforeClass
     public  void SetUP() {
